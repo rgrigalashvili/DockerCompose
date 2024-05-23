0532e88f79387e81d593c036ad3bf86b651ca65e
Ramaz Grigalashvili <Ramaz.Grigalashvili@greener.software>
2024-05-24 01:28:12 +0400

    Upload project

---
+++ Entities/Migrations/20240522172110_dbInit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace WebService.Migrations
+{
+    public partial class dbInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int(11)", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(unicode: false, maxLength: 45, nullable: false),
+                    LastName = table.Column<string>(unicode: false, maxLength: 45, nullable: false),
+                    Username = table.Column<string>(unicode: false, maxLength: 45, nullable: false),
+                    Password = table.Column<string>(unicode: false, maxLength: 45, nullable: false),
+                    EnrollmentDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user");
+        }
+    }
+}
